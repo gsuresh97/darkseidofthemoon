d73d41cea42e8bedfb853381720bd05c72bc5a05
Gopi Suresh <suresh.gopikrishnan@gmail.com>
2016-04-30 01:32:04 -0700

    plot working

--- upload.py
+++ upload.py
@@ -1,5 +1,15 @@
-!#/usr/bin/python
+import matplotlib.pyplot as plt
+from scipy.fftpack import fft
+from scipy.io import wavfile
 
-if __name__=="__main":
-    try: import scipy
-    
+song = wavfile.read('t1.wav')
+
+a = song[1] # this is a two channel soundtrack, I get the first track
+
+print a
+b=[(ele/2**8.)*2-1 for ele in a] # this is 8-bit track, b is now normalized on [-1,1)
+c = fft(b) # calculate fourier transform (complex numbers list)
+d = len(c)/2  # you only need half of the fft list (real signal symmetry)
+
+plt.plot(abs(c[:(d-1)]),'r')
+plt.show()
